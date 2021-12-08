bed7d0472c23acf4497ac4f96da742fced349915
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-07 21:33:18 -0500

    Update sploit0.c

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -7,6 +7,17 @@
 
 #define TARGET "/tmp/target0"
 
+char *repeat(char *s, int x)
+{
+  //from https://stackoverflow.com/a/22599676/5552894
+  char *result = malloc(sizeof(s) * x + 1);
+  while (x > 0) {
+      strcat(result, s);
+      x --;
+  }
+  return result;
+}
+
 int main(void)
 {
   char *args[3]; 
